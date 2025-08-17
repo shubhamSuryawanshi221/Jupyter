192a2c3b70ab8ac95840eafe8a27299b649913b5
Shubham Suryawanshi <150051910+shubhamSuryawanshi221@users.noreply.github.com>
2025-08-17 11:20:37 +0530

    .py file (for math operations)

---
+++ math_operations_new.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# math_operations.py (module)
+def add(a, b):
+    return a + b
+add(2,3)
+
+
+# In[2]:
+
+
+def square_and_cube(num):
+    square=num**2
+    cube=num**3
+    return square,cube
+
+
+# In[4]:
+
+
+square_and_cube(2)
+
